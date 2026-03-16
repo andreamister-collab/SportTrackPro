c3e4b9bb6245b892c665e3d2e7560f8bfaed4521
andreamister-collab <andrea.mister@gmail.com>
2026-03-16 01:03:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "https://TUO-PROGETTO.supabase.co"
-export const SUPABASE_ANON_KEY = "TUA_ANON_KEY"
+export const SUPABASE_URL = "https://jvcplfgozdqirzqfqwox.supabase.co"
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Y3BsZmdvemRxaXJ6cWZxd294Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1OTIxMjIsImV4cCI6MjA4OTE2ODEyMn0.-W724h8dTtaAyV1HxydYRddoD1oRxmEm0-zZc_8bxx8"
